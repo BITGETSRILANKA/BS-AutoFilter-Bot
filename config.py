8b2afb92f0d442f1f9b50af1aa001a5165774fb7
BITGETSRILANKA <kolloniyama712@gmail.com>
2026-01-15 19:37:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,17 +12,18 @@ logger = logging.getLogger("BSFilterBot")
 API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+TMDB_API_KEY = os.environ.get("TMDB_API_KEY", "") # <--- NEW
 
 # Channels & Admins
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", 0))       # Where files are uploaded
-ADMIN_ID = int(os.environ.get("ADMIN_ID", 0))           # Your ID
-FSUB_CHANNEL_ID = int(os.environ.get("FSUB_CHANNEL_ID", 0)) # Force Sub Channel ID
-FSUB_LINK = os.environ.get("FSUB_LINK", "")             # Link to Force Sub Channel
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", 0))
+ADMIN_ID = int(os.environ.get("ADMIN_ID", 0))
+FSUB_CHANNEL_ID = int(os.environ.get("FSUB_CHANNEL_ID", 0)) 
+FSUB_LINK = os.environ.get("FSUB_LINK", "")
 
 # Database
 DB_URL = os.environ.get("DB_URL", "")
-FIREBASE_KEY = os.environ.get("FIREBASE_KEY", "")       # JSON string content
+FIREBASE_KEY = os.environ.get("FIREBASE_KEY", "")
 
 # Settings
 PORT = int(os.environ.get('PORT', 8080))
-DELETE_DELAY = 120  # Seconds (2 Minutes)
+DELETE_DELAY = 120
